41dd9d476c956a21977098f404e77657df980fa4
smoesmoesmoe <smoesmoesmoe@72fadaa7-1440-0410-97bb-0f60bd0c90da>
2008-05-15 22:15:57 +0000

    Fixes to Makefile, typo in .xml, update of script

--- trunk/src/apibaso_filter.pl
+++ trunk/src/apibaso_filter.pl
@@ -1,18 +1,16 @@
 #!/usr/bin/perl -w
 
-# be careful with what you do
-use strict;
+use strict; # avoids errors that might otherwise be tolerated
 
 use Getopt::Long;
-my ($xmlfile,$format,$db,$debug)
-  =(undef,   undef,  undef, 0);
+my ($xmlfile,$format,$db,$debug,$destdir)
+  =(undef,   undef,  undef, 0,  undef);
 GetOptions("xmlfile=s"   => \$xmlfile,
            "format=s"    => \$format,
            "db=s"        => \$db,
+	   "destdir=s"	 => \$destdir,
 	   "debug"       => \$debug)
 or Getopt::Long::HelpMessage(2);
-
-
 
 # BioPerl knows how to retrieve protein sequences from the Internet
 use Bio::SeqIO;
@@ -23,6 +21,8 @@ use XML::SimpleObject;
 
 # the input file is for now set statically
 $xmlfile = 'apibaso_test.xml' unless defined($xmlfile);
+
+$destdir = "." unless defined($destdir);
 
 # a parser for the input file is created
 my $parser = XML::Parser->new(ErrorContext => 2, Style => "Tree");
@@ -40,12 +40,20 @@ eval {
 };
 die($@) unless ! $@;
 
+die "Destination directory '$destdir' does not exist.\n"
+	unless -d "$destdir";
+
+my $APICALFNAME="$destdir/apical.complete.fasta";
+my $BASOFNAME="$destdir/basolateral.complete.fasta";
+
 my $f=undef;
-if ( -r "apical.complete.fasta") {
-	unlink("apical.complete.fasta")      or die "Could not remove file apical.complete.fasta\n";
+if ( -r "$APICALFNAME" ) {
+	unlink($APICALFNAME) or die "Could not remove file $APICALFNAME\n";
+	print STDERR "Cleared file '$APICALFNAME'.\n";
 }
 if ( -r "basolateral.complete.fasta") {
-	unlink("basolateral.complete.fasta") or die "Could not remove file basolateral.complete.fasta\n";
+	unlink("$BASOFNAME") or die "Could not remove file $BASOFNAME";
+	print STDERR "Cleared file '$BASOFNAME'.\n";
 }
 
 # iterate over all entries in our data
@@ -80,11 +88,11 @@ foreach my $variant ($xso->child('api:variantlist')->children('api:variant')) {
     # schaue was in location steht
     if ($variant->attribute('location') eq "apical") {
     # output file is dies
-    	open($f, ">>apical.complete.fasta")      or die "Could not open file apical.complete.fasta for writing\n";
+    	open($f, ">>$APICALFNAME")      or die "Could not open file $APICALFNAME for writing\n";
     }
     elsif ($variant->attribute('location') eq "basolateral") {
     # sonst jenes
-	open($f,">>basolateral.complete.fasta") or die "Could not open file basolateral.complete.fasta for writing\n";
+	open($f,">>$BASOFNAME") or die "Could not open file $BASOFNAME for writing\n";
     }
     # und bei anderen Sonderfällen das
     else {
@@ -111,15 +119,34 @@ apibaso_filter -  Use XML file to retrieve entries from UniProt database.
 
 =head1 SYNOPSIS
 
-to be written
+apibaso_filter --xmlfile apibaso.xml 
 
 =head1 OPTIONS
 
-to be written
+=over 4
+
+=item xmlfile <file> : specifies the file to be parsed
+
+=format <val> : paramter expected by BioPerl, defaults to fasta
+
+=item db <name> : parameter expected by BioPerl, defaults to swissprot
+
+=item destdir <path> : specification of folder in which to store the result files
+
+=item  debug : set for increased verbosity
+
+=back
+
 
 =head1 DESCRIPTION
 
-The XML file describing protein location refers to the UniProt accession number and entry IDs. The protein sequence however is not explicitly stored. This script reads the entry and downloads the sequences online.
+The XML file describing protein location refers to the UniProt accession
+number and entry IDs. The protein sequence however is not explicitly
+stored. This script reads the entry with routines provided by the BioPerl
+library and downloads the sequences online.
+
+The script creates two files, apical.complete.fasta and basolateral.complete.fasta
+in the directory that is specified by the destdir paramter.
 
 =head1 AUTHOR
 
